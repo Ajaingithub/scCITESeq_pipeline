d2396609a637a9dc1b54900c6ab54d5727b7e629
Abhinav Jain <37553954+Ajaingithub@users.noreply.github.com>
2024-03-15 08:42:15 -0500

    Update heatmap_and_pathway_analysis.R

--- pipeline_functions/heatmap_and_pathway_analysis.R
+++ pipeline_functions/heatmap_and_pathway_analysis.R
@@ -1,3 +1,12 @@
+# This function is used for identify the markers, generating featureplots, heatmaps as well as perform Gene Ontology for the differential genes
+# obj: Seurat_object
+# saveDir: saving directory
+# ident_1: Cluster to be used for identify markers and GO
+# min_pct: minimum percentage of the features expressing within the clusters while calculating the markers
+# objname: Name of the samples
+# process: During saving what name to use
+# Assay: Which seurat assay to use to calcuate the markers
+
 cluster_markers_GSEA = function(obj, saveDir, ident_1, min_pct = 0.25, objname,process,Assay){
   library(Seurat)
   message(paste("Calculating the RNA and ADT markers for the specific cluster",ident_1, sep = ""))
@@ -30,36 +39,35 @@ cluster_markers_GSEA = function(obj, saveDir, ident_1, min_pct = 0.25, objname,p
   dir.create(paste(saveDir,"GSEA",sep = ""),showWarnings = FALSE)
   write.csv(CM_EM_marker_RNA,paste(saveDir,"GSEA/",objname,"_",process,"_",Assay,"_",ident_name,".csv",sep = ""),quote = FALSE)
   
-  # library(clusterProfiler)
-  # library(enrichplot)
-  # library(ggplot2)
-  # library(org.Hs.eg.db)
-  # 
-  # genename_symbol = rownames(CM_EM_marker_RNA)
-  # genename_entrez <- bitr(genename_symbol, fromType = "SYMBOL",
-  #                         toType = c("ENTREZID"), OrgDb = "org.Hs.eg.db") 
-  # CM_EM_marker_RNA_entrezid = CM_EM_marker_RNA[match(genename_entrez$SYMBOL,genename_symbol),]
-  # original_gene_list = CM_EM_marker_RNA_entrezid$avg_log2FC
-  # 
-  # 
-  # names(original_gene_list) <- genename_entrez$ENTREZID
-  # gene_list<-na.omit(original_gene_list)
-  # gene_list = sort(gene_list, decreasing = TRUE)
-  # 
-  # gse <- gseGO(geneList=gene_list, 
-  #              ont ="BP",
-  #              minGSSize = 3,
-  #              maxGSSize = 800,
-  #              pvalueCutoff = 0.05, 
-  #              verbose = TRUE,
-  #              OrgDb = org.Hs.eg.db,
-  #              pAdjustMethod = "none")
-  # 
-  # require(DOSE)
-  # dir.create(paste(savedir,"GSEA",sep = ""),showWarnings = F)
-  # pdf()
-  # dotplot(gse, showCategory=10, split=".sign") + facet_grid(.~.sign)
+  library(clusterProfiler)
+  library(enrichplot)
+  library(ggplot2)
+  library(org.Hs.eg.db)
+
+  genename_symbol = rownames(CM_EM_marker_RNA)
+  genename_entrez <- bitr(genename_symbol, fromType = "SYMBOL",
+                          toType = c("ENTREZID"), OrgDb = "org.Hs.eg.db")
+  CM_EM_marker_RNA_entrezid = CM_EM_marker_RNA[match(genename_entrez$SYMBOL,genename_symbol),]
+  original_gene_list = CM_EM_marker_RNA_entrezid$avg_log2FC
   
   
+  names(original_gene_list) <- genename_entrez$ENTREZID
+  gene_list<-na.omit(original_gene_list)
+  gene_list = sort(gene_list, decreasing = TRUE)
+  
+  gse <- gseGO(geneList=gene_list,
+               ont ="BP",
+               minGSSize = 3,
+               maxGSSize = 800,
+               pvalueCutoff = 0.05,
+               verbose = TRUE,
+               OrgDb = org.Hs.eg.db,
+               pAdjustMethod = "none")
+  
+  require(DOSE)
+  dir.create(paste(savedir,"GSEA",sep = ""),showWarnings = F)
+  pdf(paste(saveDir,"GSEA/gsea.pdf",sep = ""),showWarnings = FALSE)
+  dotplot(gse, showCategory=10, split=".sign") + facet_grid(.~.sign)
+  dev.off()  
   
 }
